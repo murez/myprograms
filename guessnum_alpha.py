96444baac34be5b375157b15a3ee87eda80b74c9
ottozsy <ottozsy@outlook.com>
2016-01-29 15:51:49 +0800

    Create guessnum_alpha.py

---
+++ guessnum_alpha.py
@@ -0,0 +1,10 @@
+n=123
+g=int(raw_input('Enter a number:'))
+if g==n :
+    print 'Exactly.It\'s the number I am thinking  '
+elif g<n :
+    print 'it\'s a little smaller'
+else :
+    print 'it\'s a little bigger'
+
+print 'Done'
