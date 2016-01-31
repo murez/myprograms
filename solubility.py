0787cd24212cb219737da338b855b5def2c59257
ottozsy <ottozsy@outlook.com>
2016-01-31 17:23:59 +0800

    Update solubility.py

--- solubility.py
+++ solubility.py
@@ -3,42 +3,104 @@ M=(raw_input("Please enter the mental like Fe+2,Fe+3,Mg,H,NH4 etc. : "))
 S=(raw_input("Please enter the source like SO4,Cl,NO3,OH etc. :"))
 if M=='H':
 	m=0
+        k=1
 elif M=='NH4':
 	m=1
+        k=1
 elif M=='K':
 	m=1
+        k=1
 elif M=='Na':
 	m=1
+        k=1
 elif M=='Ba':
 	m=2
+        k=2
 elif M=='Ca':
 	m=3
+        k=2
 elif M=='Mg':
 	m=4
+        k=2
 elif M=='Al':
 	m=5
+        k=3
 elif M=='Zn':
 	m=7
+        k=2
 elif M=='Fe+2':
 	m=7
+	M='(+2)Fe'
+        k=2
 elif M=='Fe+3':
 	m=7
+	M='(+3)Fe'
+        k=3
 elif M=='Cu':
 	m=7
+        k=2
 elif M=='Ag':
 	m=9
+        k=1
 
 if S=='OH':
 	s=3
+        p=1
 elif S=='NO3':
 	s=20
+        p=1
 elif S=='Cl':
 	s=8
+        p=1
 elif S=='SO4':
 	s=9
+        p=2
 elif S=='CO3':
 	s=1.5
+        p=2
 
+if k==1:
+        if p==1:
+         a='_'
+         b='_'
+        if p==2:
+         a='2'
+         b='_'
+elif k==2:
+        if p==1:
+         a='_'
+         b='2'
+        if p==2:
+         a='_'
+         b='_'
+elif k==3:
+        if p==1:
+         a='_'
+         b='3'
+        if p==2:
+         a='2'
+         b='3'
+
+if M=='NH4':
+        if p==2:
+         M='(NH4)'
+elif S=='OH':
+        if k==2:
+         S='(OH)'
+        elif k==3:
+         S='(OH)'
+elif S=='NO3':
+        if k==2:
+         S='(NO3)'
+        elif k==3:
+         S='(NO3)'
+elif S=='SO4':
+        if k==3:
+         S='(SO4)'
+elif S=='CO3':
+        if k==3:
+         S='(CO3)'
+        
 if m==2:
 	if s==8:
 		print 'Ba SO4 not solubility'
@@ -48,12 +110,15 @@ elif m==4:
 elif m==5:
 	if s==1.5:
 		print'no this kind of material'
+elif m==9:
+ if s==3:
+  print'no this kind of material'
 elif m-s<0:
-	print M+S+'solubility'
+	print M+a+S+b+'solubility'
 elif m-s==0:
-	print M+S+'little solubility'
+	print M+a+S+b+'little solubility'
 elif m-s>0:
-	print M+S+'no solubility'
+	print M+a+S+b+'no solubility'
 else :
 	print'Try again'
 #made by ZhangSiyuan
